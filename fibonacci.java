89751cc9aeab494c899096a740304ffd511639e8
Tu Nombre <tu_correo@example.com>
2024-10-19 13:13:31 -0500

    Subir tarea

---
+++ fibonacci.java
@@ -0,0 +1,15 @@
+/*
+ * Tarea N.1
+ * Nombre: Niurka Vanesa Yupanqui
+ * Curso: GR3SW
+ * Materia: PROGRAMACIÓN II
+ * 
+ * Escribir un programa en Java que permita identificar si un n´umero
+ * ingresado por el usuario, pertenece a la Secuencia de Fibonacci. Si
+ * el n´umero pertenece a la secuencia, se debe mostrar el mensaje ✭✭El
+ * n´umero pertenece a la Secuencia de Fibonacci✮✮, caso contrario, se debe
+ * mostrar el mensaje ✭✭El n´umero no pertenece a la serie de Fibonacci✮✮
+ * 
+ * 
+ * 
+ */
